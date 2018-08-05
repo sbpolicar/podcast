5ecd0b4a43a4309805cd78d227d5488fe8639834
sbpolicar <sbpolicar@gmail.com>
2018-08-04 22:15:32 -0700

    Init work and setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {
+  Routes,
+  RouterModule
+} from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PodcastsComponent } from './components/podcasts/podcasts.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'podcasts',
+    component: PodcastsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
