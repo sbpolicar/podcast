81ad1aefd75bed55e4c33337223a72127677c6ca
Sam Policar <Sam@Sams-MacBook-Pro.local>
2018-08-26 20:01:18 -0700

    Final cut 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,6 @@ import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit {
-  expanded: boolean = false;
-
-  title = 'app';
-  
   ngAfterViewInit() {
-    console.log('height---' + this.contentCard.nativeElement.offsetHeight);
-    console.log('width---' + this.contentCard.nativeElement.offsetWidth);
   }
-
-  @ViewChild('contactFormFrame')
-  contentCard: ElementRef;
 }
